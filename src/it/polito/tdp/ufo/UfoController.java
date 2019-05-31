811f1dea65d284cd58309f1db0b068404bbe799d
albertomonge <alberto.monge@polito.it>
2019-05-31 11:33:33 +0200

    Progetto Base

---
+++ src/it/polito/tdp/ufo/UfoController.java
@@ -0,0 +1,53 @@
+/**
+ * Sample Skeleton for 'Ufo.fxml' Controller Class
+ */
+
+package it.polito.tdp.ufo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+public class UfoController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="boxAnno"
+    private ComboBox<?> boxAnno; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxStato"
+    private ComboBox<?> boxStato; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void handleAnalizza(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleAvvistamenti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSequenza(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert boxAnno != null : "fx:id=\"boxAnno\" was not injected: check your FXML file 'Ufo.fxml'.";
+        assert boxStato != null : "fx:id=\"boxStato\" was not injected: check your FXML file 'Ufo.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Ufo.fxml'.";
+
+    }
+}
